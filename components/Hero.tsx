52c34c26abbc1f57c22d752d553cd5f7b545d48c
Aneer-Thapa1 <anir234thapa@gmail.com>
2024-07-27 16:36:54 +0545

    before getting error

---
+++ components/Hero.tsx
@@ -0,0 +1,15 @@
+import { Spotlight } from "./ui/Spotlight";
+import { SparklesCore } from "./ui/sparkles";
+
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36">
+      <div>
+        <Spotlight />
+        <SparklesCore />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
