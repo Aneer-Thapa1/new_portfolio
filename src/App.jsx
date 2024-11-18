48c66bf45879c84db43719681d10ee5dc77499c1
Anir Jung Thapa <134839446+Aneer-Thapa1@users.noreply.github.com>
2024-11-18 21:25:55 +0545

    restart

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,7 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <div className="bg-black">
-      <Navbar />
-      <Hero />
-    </div>
-  );
+  return <div className="bg-black">Hi</div>;
 }
 
 export default App;
